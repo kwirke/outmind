f5d138acacd3e57f622bb0324a5de60e80b2e968
Marc Rodríguez <nebrios2@gmail.com>
2018-02-06 08:35:35 +0100

    Structure bundling

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "plugins": [
+        "fp"
+    ],
+    "extends": "plugin:fp/recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "fp/no-arguments": "error",
+        "fp/no-class": "error",
+        "fp/no-delete": "error",
+        "fp/no-events": "error",
+        "fp/no-get-set": "error",
+        "fp/no-let": "error",
+        "fp/no-loops": "error",
+        "fp/no-mutating-assign": "error",
+        "fp/no-mutating-methods": "error",
+        "fp/no-mutation": "error",
+        "fp/no-nil": "error",
+        "fp/no-proxy": "error",
+        "fp/no-rest-parameters": "error",
+        "fp/no-this": "error",
+        "fp/no-throw": "error",
+        "fp/no-unused-expression": "error",
+        "fp/no-valueof-field": "error",
+        "no-var": "error"
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+const img = document.getElementById('background');
+const aux = ctx.drawImage(img, 0, 0);
